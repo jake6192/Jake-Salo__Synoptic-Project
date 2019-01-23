827abfe701bd4981371d8d21eb77ecaef86426a9
jake6192 <jakesalo3914@gmail.com>
2019-01-23 13:21:42 +0000

    Draws passages for the room.
     -Also modified config.json slightly to differentiate testing rooms.

--- js/Room.js
+++ js/Room.js
@@ -5,6 +5,7 @@ class Room {
     this.Treasure = room.Treasure;
     this.Threat = room.Threat;
     this.size = 300; // Size of all rooms in pixels. //
+    this.passageSize = this.size/10;
     this.margins = { // Used to center the room in the canvas. //
       hMargin: (GAME.canvas.width-this.size)/2,
       vMargin: (GAME.canvas.height-this.size)/2
@@ -23,6 +24,18 @@ class Room {
       /* Fill the room background. */
       GAME.context.fillStyle = '#999999';
       GAME.context.fillRect(2, 2, this.size-4, this.size-4);
+
+      /* Draw any passages for the room. */
+      GAME.context.fillStyle = '#555555';
+      if(this.Passages.North.passageIsOpen) GAME.context.fillRect((this.size-this.passageSize)/2, 1, this.passageSize, this.passageSize);
+      if(this.Passages.East.passageIsOpen) GAME.context.fillRect((this.size-this.passageSize)-1, (this.size-this.passageSize)/2, this.passageSize, this.passageSize);
+      if(this.Passages.South.passageIsOpen) GAME.context.fillRect((this.size-this.passageSize)/2, (this.size-this.passageSize)-1, this.passageSize, this.passageSize);
+      if(this.Passages.West.passageIsOpen) GAME.context.fillRect(1, (this.size-this.passageSize)/2, this.passageSize, this.passageSize);
+
+      /* Draw the threats for this room. */
+
+      /* Draw the treasure for this room. */
+
     };
   };
 }
