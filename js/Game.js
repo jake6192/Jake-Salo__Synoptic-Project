546a6227f1e823e0a701c7599683990a74907630
jake6192 <jakesalo3914@gmail.com>
2019-01-23 12:46:23 +0000

    Creates a blank canvas on page load

--- js/Game.js
+++ js/Game.js
@@ -2,7 +2,10 @@ class Game {
   constructor() {
     this.config = null;
     this.rooms = [];
+    this.canvasWidth = 1200;
+    this.canvasHeight = 675;
 
+    /* loadConfig() will pull the tempory configuration file and assign it's contents to the GAME object. */
     this.loadConfig = function(path, callback) {
       $.getJSON(path, function(result) {
         GAME.config = result;
@@ -15,6 +18,22 @@ class Game {
       for(key in this.config) len += (this.config.hasOwnProperty(key) ? 1 : 0);
       return len;
     };
+
+
+    this.prepareCanvas = function() {
+      let canvas = document.createElement('canvas');
+      let context = canvas.getContext('2d');
+      canvas.id = "canvas";
+      canvas.width = this.canvasWidth;
+      canvas.height = this.canvasHeight;
+      /* Draw the canvas background. */
+      context.fillStyle = '#BBBBBB';
+      context.fillRect(0, 0, canvas.width, canvas.height);
+      /* Insert the canvas into the HTML markup. */
+      document.getElementById('mainContainer').innerHTML = '';
+      document.getElementById('mainContainer').appendChild(canvas);
+    };
+
 
     this.createMaze = function() {
       for(let i = 0; i < GAME.getConfigLength(); i++) {
--- js/app.js
+++ js/app.js
@@ -1,9 +1,19 @@
+/*
+* These global constants will contain all game information
+* and will be referenced throughout the program
+**/
 const GAME = new Game();
 const PLAYER = new Player();
 
+
+/*
+* When the DOM has loaded, begin preparing components for the maze.
+**/
 $(document).ready(function() {
+  /* This uses a callback function as all of the other processes are dependant on the configuration information. */
   GAME.loadConfig('config.json', function() {
     GAME.createMaze();
-    PLAYER.setRoom();
+    GAME.prepareCanvas(); // Draws a blank canvas in preparation for rooms. //
+    PLAYER.setRoom(); // Assign the player to a random room. //
   });
 });
